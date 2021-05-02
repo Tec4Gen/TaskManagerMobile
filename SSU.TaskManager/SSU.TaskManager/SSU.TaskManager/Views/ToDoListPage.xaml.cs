893c56c0df0f959f940eb0ccdf0487c02da77296
Дмитрий Кузьмин <dima251200@yandex.ru>
2021-05-02 13:20:49 +0400

    Deleted garbage

--- SSU.TaskManager/SSU.TaskManager/SSU.TaskManager/Views/ToDoListPage.xaml.cs
+++ SSU.TaskManager/SSU.TaskManager/SSU.TaskManager/Views/ToDoListPage.xaml.cs
@@ -21,5 +21,30 @@ namespace SSU.TaskManager.Views
         {
             
         }
+
+        public async void OnBtClick(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ContentPage
+            {
+                Content = new StackLayout
+                {
+                    VerticalOptions = LayoutOptions.Center,
+                    Children = {
+                        new Button {
+
+                            Text = "button1"
+                        },
+                        new Button {
+
+                           Text = "button2"
+                        },
+                        new Button {
+
+                            Text = "button3"
+                        }
+                    }
+                }
+            });
+        }
     }
 }
